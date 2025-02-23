26988d1fd53b081d09b070cae5835f5d6d8bec49
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-23 13:13:14 +0100

    add error handling

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-  fetch('https://reqres.in/api/users')
-       .then(res => console.log(res) )
+  fetch('https://reqres.in/api/users/23'),{ 
+    method : 'POST'
+  }
+       .then(res =>{
+         if(res.ok){
+            console.log('SUCCESS');
             
+         }else{
+            console.log('not success');
+            
+         }
+       } )
+        .then(data => console.log(data) )    
+        .catch(error => console.log('ERROR')
+        )
